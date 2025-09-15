349898deb2a14b0120b8e8c8c1b9fc1cfb71c90f
seu nome de usuário <pealq63@gmail.com>
2025-09-15 16:19:43 -0300

    adicionado navegação por Link no offCanvas

--- src/componentes/OffCanvas/index.jsx
+++ src/componentes/OffCanvas/index.jsx
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
 import './offCanvas.css'
 
 function OffCanvas(){
+    console.log(location)
     return(
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+        <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
             <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
+                <h5 class="offcanvas-title" id="offcanvasRightLabel">Menu</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
-            <div class="offcanvas-body">
-            ...
+            <div className="offcanvas-body">
+                <Link className="offCanvas-link" to={'/'}>Inicio</Link>
+                <Link className="offCanvas-link" to={'/cardapio'}>Cardapio</Link>
             </div>
         </div>
     )
