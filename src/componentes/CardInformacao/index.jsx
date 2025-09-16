ce0d618e0d93cf640137e2126df2b2ec5b2e4d29
seu nome de usuário <pealq63@gmail.com>
2025-09-16 15:53:09 -0300

    criado os card de informação da landing page

--- src/componentes/CardInformacao/index.jsx
+++ src/componentes/CardInformacao/index.jsx
@@ -1,7 +1,14 @@
-import './CardInformacao';
+import './CardInformacao.css';
 
-function CardInformacao(){
-    return(<h1>card</h1>)
+function CardInformacao({ srcImg, titulo, texto, className, alt }){
+    return(
+        <div className={className}>
+            <img className='logo-img' src={srcImg} alt={alt}/>
+            <h4 className='cardInfo-titulo'>{titulo}</h4>
+            <p className='cardInfo-texto'>{texto}</p>
+        </div>
+        
+    )
 }
 
 export default CardInformacao
--- src/paginas/LandingPage/index.jsx
+++ src/paginas/LandingPage/index.jsx
@@ -1,6 +1,7 @@
-import Banner from '../../componentes/Banner';
-import Faixa from '../../componentes/Faixa';
 import './landingPage.css';
+import Banner from '../../componentes/Banner';
+import CardInformacao from '../../componentes/CardInformacao';
+import Faixa from '../../componentes/Faixa';
 
 function LandingPage(){
     return (
@@ -16,6 +17,29 @@ function LandingPage(){
                     texto={'Vem entender por que nossos doces'}
                     texto2={'conquistam corações'}
                 />
+                <div className='container-cardInformacao'>
+                    <CardInformacao
+                        srcImg={'./src/assets/bolo.png'}
+                        titulo={'Encomendas para festas e eventos.'}
+                        texto={'Transforme sua comemoração em um momento inesquecível com nossos doces feitos sob medida para encantar seus convidados.'}
+                        className={'card-informacao1'}
+                        alt={'logo de uma fatia de bolo'}
+                    />
+                    <CardInformacao
+                        srcImg={'./src/assets/celular.png'}
+                        titulo={'Faça seu pedido pelo WhatsApp e receba com conforto e praticidade.'}
+                        texto={'Peça seus doces favoritos sem sair de casa. Atendimento rápido, direto e cheio de carinho — do jeitinho que você merece.'}
+                        className={'card-informacao2'}
+                        alt={'logo de um celular'}
+                    />
+                    <CardInformacao
+                        srcImg={'./src/assets/brigadeiro.png'}
+                        titulo={'Doces artesanais com aquele gostinho de feito em casa..'}
+                        texto={'Receitas preparadas com carinho, ingredientes frescos e o sabor autêntico que traz lembranças boas em cada mordida.'}
+                        className={'card-informacao3'}
+                        alt={'logo de um brigadeiro'}
+                    />
+                </div>
             </section>
         </main>
         
