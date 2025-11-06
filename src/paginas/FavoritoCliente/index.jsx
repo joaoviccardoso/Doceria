08f5f5b0a1d2d791f50ab84ecb45f6baef233130
seu nome de usuário <pealq63@gmail.com>
2025-11-06 16:32:54 -0300

    feito display flex na pagina favoritos

--- src/paginas/FavoritoCliente/index.jsx
+++ src/paginas/FavoritoCliente/index.jsx
@@ -1,7 +1,46 @@
+
+import { useEffect, useState } from 'react'
+import InformacoesCliente from '../../componentes/InformacaoDoCliente'
+import MenuCliente from '../../componentes/menuCliente'
 import './favoritosCliente.css'
+import Card from '../../componentes/Card'
 
 function Favoritos(){
-    return(<h1>Meus Favoritos</h1>)
+    const [favoritos, setFavoritos] = useState([])
+
+    useEffect( () => {
+        async function PegarListaFavoritos(){
+            const dados = await fetch('/data/favoritos.json');
+            if(!dados.ok){
+                alert('erro para carregar os favoritos')
+            }
+            const itensFavoritos = await dados.json()
+            setFavoritos(itensFavoritos)
+            console.log(itensFavoritos)
+        }
+
+        PegarListaFavoritos()
+    },[])
+
+    return(
+       <main className='main-favoritos'>
+            <section className='container-navegacaoCliente'>
+                <MenuCliente/>
+            </section>
+
+            <section className='container-produtosFavoritos'>
+                <InformacoesCliente/>
+
+                <section className='container-favoritos'>
+                    {favoritos.map(favorito => (
+                        <Card
+                            produto={favorito}
+                        />
+                    ))}
+                </section>
+            </section>
+       </main> 
+    )
 }
 
 export default Favoritos
